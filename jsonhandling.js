ffbc3432cbf994371640396c03bc7a80ca782182
insky2 <bujtoru@gmail.com>
2023-04-21 07:07:48 +0200

    Changes to be committed:  	modified:   Chocolate_Cake.html  	modified:   INDEX.html  	modified:   explore.html  	modified:   jsonhandling.js  	deleted:    libary.html  	deleted:    minsearchtest.html  	deleted:    navbar.html  	modified:   recipes.json  	modified:   scripts.js  	deleted:    test.html

--- jsonhandling.js
+++ jsonhandling.js
@@ -51,3 +51,4 @@ function populateRecipePage(recipe) {
   });
 }
 
+
--- scripts.js
+++ scripts.js
@@ -7,31 +7,3 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-// check if recipe is saved in localStorage
-var recipeJson = localStorage.getItem('recipe');
-if (recipeJson) {
-  var recipeObj = JSON.parse(recipeJson);
-  if (recipeObj.saved === 'y') {
-    // assign the "active" class to the link with href="library.html"
-    var libraryLink = document.querySelector('a[href="library.html"]');
-    libraryLink.classList.add('active');
-  } else {
-    // assign the "active" class to the link with href="explore.html"
-    var exploreLink = document.querySelector('a[href="explore.html"]');
-    exploreLink.classList.add('active');
-  }
-} else {
-  // get the current URL
-  var currentUrl = window.location.href;
-
-  // get all the links in the navigation bar
-  var links = document.querySelectorAll('.topnav a');
-
-  // loop through the links and check if the URL matches
-  for (var i = 0; i < links.length; i++) {
-    var linkUrl = links[i].href;
-    if (currentUrl === linkUrl) {
-      links[i].classList.add('active');
-    }
-  }
-}
